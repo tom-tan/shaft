eadf37677455fd4d17677c56b762db1e08fcc77b
Tomoya Tanjo <ttanjo@gmail.com>
2022-04-24 00:10:35 +0000

    WIP

--- alpine-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-apk --no-cache add dub ldc gcc musl-dev git llvm-libunwind-static
-
-git config --global --add safe.directory $PWD
-
-dub --cache=local build -b release-static || exit 1
-strip bin/shaft
