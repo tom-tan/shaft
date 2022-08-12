53763d8c0bf7f4acc1317b7dbd44a0e4ccb13afd
Tomoya Tanjo <ttanjo@gmail.com>
2022-08-12 16:16:27 +0000

    Tweaks

--- source/shaft/evaluator/engine/embedded.d
+++
@@ -1,8 +0,0 @@
-/**
- * This file is overwritten by dpp in `preBuildCommand` to prevent link errer during `dub build`.
- *
- * Authors: Tomoya Tanjo
- * Copyright: © 2022 Tomoya Tanjo
- * License: Apache-2.0
- */
-module shaft.evaluator.engine.embedded;
