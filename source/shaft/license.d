78dd024b52141b458080c3b4beaf5618ee029f77
Tomoya Tanjo <ttanjo@gmail.com>
2022-08-13 13:45:50 +0000

    Tentatively add a license notice for musl libc

--- source/shaft/license.d
+++ source/shaft/license.d
@@ -5,12 +5,38 @@
  */
 module shaft.license;
 
+import std.algorithm : map;
 import std.format : format;
-import std.string : chomp;
+import std.string : chomp, join;
+
+enum thirdPartiesBase = [
+    LicenseInfo(
+        "njs",
+        "https://github.com/nginx/njs",
+        import("license-njs.txt").chomp,
+    ),
+];
+
+static if (import("license-musl.txt").length == 0)
+{
+    // dynamic link
+    enum thirdParties = thirdPartiesBase;
+}
+else
+{
+    // static link
+    enum thirdParties = [
+        LicenseInfo(
+            "musl libc",
+            "https://musl.libc.org/",
+            import("license-musl.txt").chomp,
+        ),
+    ] ~ thirdPartiesBase;
+}
 
 enum licenseString = format!q"EOS
 # NOTICES AND INFORMATION
-This software is licensed under Apache-2.0 License.
+This software is licensed under the following Apache-2.0 License.
 
 ```
 %s
@@ -19,15 +45,31 @@ This software is licensed under Apache-2.0 License.
 # THIRD-PARTY SOFTWARE NOTICES AND INFORMATION
 This software incorporates material from the following third party.
 
-## njs
+%s
+EOS"(
+    import("license-shaft.txt").chomp,
+    thirdParties.map!(a => a.toString).join("\n"),
+);
 
-Link: https://github.com/nginx/njs
+struct LicenseInfo
+{
+    string name;
+    string link;
+    string license;
+
+    auto toString() const pure scope
+    {
+        import std.format : format;
+
+        return format!q"EOS
+## %s
+
+Link: %s
 
 ### License
 ```
 %s
 ```
-EOS"(
-    import("license-shaft.txt").chomp,
-    import("license-njs.txt").chomp
-);
+EOS"(name, link, license);
+    }
+}
