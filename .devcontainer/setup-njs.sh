af650097ab5bac11b14a55a9fd9ef2bee3df30b1
Tomoya Tanjo <ttanjo@gmail.com>
2024-03-29 17:23:26 +0000

    Fix setup-njs.sh

--- .devcontainer/setup-njs.sh
+++ .devcontainer/setup-njs.sh
@@ -15,7 +15,7 @@ git clone --depth 1 https://github.com/nginx/njs.git -b $version njs-repo
 
 cd njs-repo
 
-./configure --no-openssl
+./configure --no-openssl --no-libxml2
 sed -e 's/^#define NJS_HAVE_UNSIGNED_INT128  1//' build/njs_auto_config.h > njs_auto_config_tmp1.h
 sed -e 's/^#define NJS_HAVE_DENORMALS_CONTROL  1//' njs_auto_config_tmp1.h > njs_auto_config_tmp2.h
 sed -e 's/^#define NJS_HAVE_BUILTIN_CLZLL  1//' njs_auto_config_tmp2.h > njs_auto_config_tmp3.h
