dba2aac6da3872173c946b625dccc7dbe428ad59
Tomoya Tanjo <ttanjo@gmail.com>
2024-04-04 16:01:13 +0900

    Update cloneCWL.sh for cwl-v1.1 and cwl-v1.2

--- .devcontainer/cloneCWL.sh
+++ .devcontainer/cloneCWL.sh
@@ -1,3 +1,21 @@
 #!/bin/sh
 
-test -d cwl-v1.0 || git clone --depth 1 https://github.com/common-workflow-language/common-workflow-language.git cwl-v1.0
+CWL_BASE_URL=https://github.com/common-workflow-language
+
+if [ -d cwl-v1.0 ]; then
+    git -C cwl-v1.0 pull
+else
+    git clone --depth 1 $CWL_BASE_URL/common-workflow-language.git cwl-v1.0
+fi
+
+if [ -d cwl-v1.1 ]; then
+    git -C cwl-v1.1 pull
+else
+    git clone --depth 1 $CWL_BASE_URL/cwl-v1.1.git cwl-v1.1
+fi
+
+if [ -d cwl-v1.2 ]; then
+    git -C cwl-v1.2 pull
+else
+    git clone --depth 1 $CWL_BASE_URL/cwl-v1.2.git cwl-v1.2
+fi
