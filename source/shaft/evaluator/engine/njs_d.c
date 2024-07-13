5f5e1bc05a728e357fa00949f4a79e4cc11ecd6a
Tomoya Tanjo <ttanjo@gmail.com>
2024-07-13 09:15:00 +0000

    Update dependencies

--- source/shaft/evaluator/engine/njs_d.c
+++ source/shaft/evaluator/engine/njs_d.c
@@ -1,5 +1,10 @@
 
 __import core.stdc.math;
-#define __builtin_signbit signbit
+
+// dmd 2.107.1 (ldc 1.37.0) and earlier
+//#define __builtin_signbit signbit
+
+// dmd 2.108.0 (ldc 1.38.0) and later
+#define __builtin_signbit __signbit
 
 #include <njs_main.h>
